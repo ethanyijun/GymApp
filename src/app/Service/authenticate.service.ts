2c3d9c05e5cc4e9a2a4baf7fbc3c3df15dd6d1f0
Yijun Gai <ethan.yijun@gmail.com>
2018-10-30 10:56:53 +1100

    restructure files, add plan hint in register new customer

---
+++ src/app/Service/authenticate.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { User } from '../Model/user';
+import { Token } from '../Model/token';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticateService {
+
+   storageKey = 'authenticate-jwt';
+   loginUrl = '/login';
+   users: User[];
+
+  constructor(private router: Router,
+              private http: HttpClient) { }
+
+  getAuthorizationOptions() {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + this.getToken()
+      })
+    };
+  }
+
+  post(user: User): Observable<Token> {
+    console.log('posting');
+    console.log(user);
+    return this.http.post<Token>(this.loginUrl, user);
+  }
+
+  setToken(token: string) {
+    localStorage.setItem(this.storageKey, token);
+  }
+
+  getToken() {
+    //console.log('getting token');
+    return localStorage.getItem(this.storageKey);
+  }
+
+  isLoggedIn() {
+    return this.getToken() != null;
+  }
+
+  isLoggedOut() {
+    return this.getToken() == null;
+  }
+
+  logOut() {
+    localStorage.removeItem(this.storageKey);
+    console.log(this.storageKey);
+    this.router.navigate(['/login']);
+  }
+}
